03608d3807d81a3699fa83e413ca0ed82d4267f6
Alumnos <Alumnos@PC179>
2022-11-09 14:01:14 -0300

    sdasaddsa

--- pryTorresIEFIPL2-LAB2/frmPrincipal.Designer.cs
+++ pryTorresIEFIPL2-LAB2/frmPrincipal.Designer.cs
@@ -161,6 +161,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(895, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.statusPrincipal);
